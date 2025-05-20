b74d04e17d5849ac051c88e6bfecd2cf1ec6bc4b
Amit Jain <129047199+crash484@users.noreply.github.com>
2025-05-20 20:33:34 +0530

    added finally block to prevent errors

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -25,13 +25,14 @@ router.post('/register',async (req,res)=>{
     const hashed = await bcrypt.hash(password, 10); //hashing the password
     const user = new User({ username, password: hashed, isAdmin }); //creating new object to store in db
     const result = await collection.insertOne(user);
-    client.close();
 
     if (result) return res.status(201).json({ message: 'User registered' });
     else return res.status(403).json({ message: 'unable to register user'})
     } catch (err) {
     console.error('Error saving user:', err);
     return res.status(500).json({ error: err.message });
+  }finally{
+    await client.close();
   }
 });
 
@@ -41,7 +42,6 @@ router.post('/register',async (req,res)=>{
       await client.connect();
       const {username,password}=req.body;
       const user = await DB.collection('Users').findOne({username: username});
-      client.close();
       if( !user  ) {
         return res.status( 401 ).json( { message : "user doesnt exist" } );
       }
@@ -54,7 +54,9 @@ router.post('/register',async (req,res)=>{
     } catch (err) {
       console.error("Error unable to find user");
       return res.status(500).json({ error: err.message });
-    } 
+    } finally{
+      await client.close();
+    }
   })
 
   //implement the jwt here

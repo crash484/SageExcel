6b0c696dae87ecbcdaa15dfddf2bca4c2447d107
Amit Jain <129047199+crash484@users.noreply.github.com>
2025-05-22 22:38:26 +0530

    backend is connected to frontend login and register work

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv"
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
 import User from "../models/user.model.js"
-import { MongoClient, ServerApiVersion } from "mongodb";
 
 const router = express.Router();
 dotenv.config();
@@ -15,29 +14,35 @@ const key=process.env.SECRET_KEY;
 //signup
 router.post('/register',async (req,res)=>{
     try{
-
-    const {username,password,isAdmin} = req.body;
+    console.log(req.body);
+    const {name,email,password} = req.body;
+    //create check if user with email already exists or not
     const hashed = await bcrypt.hash(password, 10); //hashing the password
-    const user = new User({ username, password: hashed, isAdmin }); //creating new object to store in db
+
+    const user = new User({ name,email, password: hashed }); //creating new object to store in db
+
     const result = await user.save();
 
-    if (result)  res.status(201).json({ message: 'User registered' });
-    else  return res.status(403).json({ message: 'unable to register user'})
+    if (result) {
+      return res.status(201).json({ message: 'User registered' });
+    }
+    else {
+      return res.status(403).json({ message: 'unable to register user'})
+    }
     } catch (err) {
     console.error('Error saving user:', err);
-     res.status(500).json({ error: err.message });
+    res.status(500).json({ error: err.message });
   }
 });
 
 //signin
   router.post('/login', async (req,res)=>{
     try{
-      const {username,password}=req.body;
-      const user = await User.findOne({username});
+      const {email,password}=req.body;
+      const user = await User.findOne({email});
       if( !user  ) {
          res.status( 401 ).json( { message : "user doesnt exist" } );
          //refresh the page
-         return;
       }
 
       //comparing the password
@@ -51,7 +56,8 @@ router.post('/register',async (req,res)=>{
           // //jwt is created after verifying the user credentials are correct
           jwt.sign(user,key,{expiresIn:'1h'},(err,token)=>{
             if(err) console.log(err);
-            res.send()
+            //need to send token
+            return;
           })
       }
     } catch (err) {
@@ -60,4 +66,6 @@ router.post('/register',async (req,res)=>{
     }
   })
 
+  
+  
 export default router;
--- server/server.js
+++ server/server.js
@@ -2,13 +2,21 @@ import express from "express";
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.routes.js"
 import mongoose from "mongoose"
+import cors from "cors"
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin: 'http://localhost:5173', // frontend origin allowed
+  credentials: true // if you use cookies or authentication headers
+}));
 app.use("/api/auth",authRoutes);
+
+
 
 mongoose.connect(process.env.MONGO_URI)
         .then(()=>{

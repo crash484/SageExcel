ffb8b379afa9bd543b2f5c73e0419626a81254b5
SNKPSEUDO11 <snk20041130@gmail.com>
2025-06-11 11:17:15 +0530

    Removed 3D charts functionality. Implemented Later

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -197,7 +197,7 @@ export default function History() {
                     initial={{ y: -30, opacity: 0 }}
                     animate={{ y: 0, opacity: 1 }}
                 >
-                    Excel Analytics
+                    SageExcel
                 </motion.h1>
                 <motion.p
                     className="text-lg text-gray-600 dark:text-gray-300"

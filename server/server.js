052341061daca03677d8413238175d917dea6b55
Shashwat Jain <129047199+crash484@users.noreply.github.com>
2025-06-22 00:31:48 +0530

    adding backend files

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import cors from "cors"
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT;
+const port = 7860;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
